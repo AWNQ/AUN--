4d4ff41003b7ca22731c7c1c8df908ec7ea417ee
islem <im_karabernou@esi.dz>
2023-04-29 17:59:55 +0100

    added a folder for the dashboard

---
+++ AWNQ/material-kit-react/src/pages/index.js
@@ -0,0 +1,158 @@
+import Head from 'next/head';
+import { subDays, subHours } from 'date-fns';
+import { Box, Container, Unstable_Grid2 as Grid } from '@mui/material';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import { OverviewBudget } from 'src/sections/overview/overview-budget';
+import { OverviewLatestOrders } from 'src/sections/overview/overview-latest-orders';
+import { OverviewLatestProducts } from 'src/sections/overview/overview-latest-products';
+import { OverviewSales } from 'src/sections/overview/overview-sales';
+import { OverviewTasksProgress } from 'src/sections/overview/overview-tasks-progress';
+import { OverviewTotalCustomers } from 'src/sections/overview/overview-total-customers';
+import { OverviewTotalProfit } from 'src/sections/overview/overview-total-profit';
+import { OverviewTraffic } from 'src/sections/overview/overview-traffic';
+import App from 'src/pages/map/App';
+
+const now = new Date();
+
+const Page = () => (
+  <>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8,
+      }}
+    >
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
+          {/*<Grid
+            xs={12}
+            lg={8}
+          >
+            <OverviewSales
+              chartSeries={[
+                {
+                  name: 'This year',
+                  data: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20]
+                },
+                {
+                  name: 'Last year',
+                  data: [12, 11, 4, 6, 2, 9, 9, 10, 11, 12, 13, 13]
+                }
+              ]}
+              sx={{ height: '100%' }}
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            md={6}
+            lg={4}
+          >
+            <OverviewTraffic
+              chartSeries={[63, 15, 22]}
+              labels={['Desktop', 'Tablet', 'Phone']}
+              sx={{ height: '100%' }}
+            />
+            </Grid>*/}
+          {/*<Grid
+            xs={12}
+            md={6}
+            lg={4}
+          >
+            <OverviewLatestProducts
+              products={[
+                {
+                  id: '5ece2c077e39da27658aa8a9',
+                  image: '/assets/products/product-1.png',
+                  name: 'Healthcare Erbology',
+                  updatedAt: subHours(now, 6).getTime()
+                },
+                {
+                  id: '5ece2c0d16f70bff2cf86cd8',
+                  image: '/assets/products/product-2.png',
+                  name: 'Makeup Lancome Rouge',
+                  updatedAt: subDays(subHours(now, 8), 2).getTime()
+                },
+                {
+                  id: 'b393ce1b09c1254c3a92c827',
+                  image: '/assets/products/product-5.png',
+                  name: 'Skincare Soja CO',
+                  updatedAt: subDays(subHours(now, 1), 1).getTime()
+                },
+                {
+                  id: 'a6ede15670da63f49f752c89',
+                  image: '/assets/products/product-6.png',
+                  name: 'Makeup Lipstick',
+                  updatedAt: subDays(subHours(now, 3), 3).getTime()
+                },
+                {
+                  id: 'bcad5524fe3a2f8f8620ceda',
+                  image: '/assets/products/product-7.png',
+                  name: 'Healthcare Ritual',
+                  updatedAt: subDays(subHours(now, 5), 6).getTime()
+                }
+              ]}
+              sx={{ height: '100%' }}
+            />
+            </Grid>*/}
+        
+          <Grid xs={12} md={12} lg={12}>
+            <Map></Map>
+          </Grid>
+          
+          <Grid xs={12} md={12} lg={12}>
+            <OverviewLatestOrders
+              orders={[
+                {
+                  id: "1",
+                  address: "25",
+                  avatar: "jade",
+                  createdAt: subDays(subHours(now, 7), 1).getTime(),
+                  email: "van",
+                  name: "A1231",
+                  phone: "...",
+                },
+                {
+                  id: "2",
+                  address: "4",
+                  avatar: "sakeeth",
+                  createdAt: subDays(subHours(now, 7), 1).getTime(),
+                  email: "car",
+                  name: "D3341",
+                  phone: "...",
+                },
+                {
+                  id: "3",
+                  address: "19",
+                  avatar: "aziz",
+                  createdAt: subDays(subHours(now, 7), 1).getTime(),
+                  email: "van",
+                  name: "Y2390",
+                  phone: "...",
+                },
+                {
+                  id: "4",
+                  address: "3",
+                  avatar: "priya",
+                  createdAt: subDays(subHours(now, 7), 1).getTime(),
+                  email: "car",
+                  name: "B1235",
+                  phone: "...",
+                },
+              ]}
+              sx={{ height: "100%" }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Page;
